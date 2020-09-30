60466926287ad5a6a9bfc9aacb2f640c897939ea
ahnafpie <ahnafurp@gmail.com>
2020-09-30 23:37:19 +0600

    Status modified

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 import express from "express";
 const app = express();
-const bodyParser    = require("body-parser");
-const mongoose    = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1/ecommerce", {
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://127.0.0.1/ecommerce", {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(() => {
-        console.log('Connected to DB!');
-
-    })
-    .catch(error => console.log(error.message));
-app.use(bodyParser.urlencoded({ extended: true })); 
-   
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to DB!");
+  })
+  .catch((error) => console.log(error.message));
+app.use(bodyParser.urlencoded({ extended: true }));
