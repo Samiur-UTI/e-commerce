8dfa497beafda2dc69c4ce6731e2b829c98872da
ahnafpie <ahnafurp@gmail.com>
2020-10-04 22:25:21 +0600

    Dashboard Layout Updated

--- client/src/components/admin/layout/Dashboard.js
+++ client/src/components/admin/layout/Dashboard.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import useWindowDimensions from "../../../hooks/UseDimension";
 import NavigationBar from "./NavigationBar";
 
 const Dashboard = () => {
-  return (
-    <Container fluid>
-      <Row>
-        <Col lg={2} md={3} sm={12}>EasyCart</Col>
-        <Col lg={10} md={9}sm={0}>
-          <NavigationBar />
-        </Col>
-      </Row>
-    </Container>
-  );
+  const { windowsWidth } = useWindowDimensions();
+
+  const onWidthChange = () => {
+    if (windowsWidth < 768) {
+      return (
+        <Container fluid>
+          <Row>
+            <Col>EasyCart</Col>
+          </Row>
+        </Container>
+      );
+    } else {
+      return (
+        <Container fluid>
+          <Row>
+            <Col lg={2} md={3}>
+              EasyCart
+            </Col>
+            <Col lg={10} md={9}>
+              <NavigationBar />
+            </Col>
+          </Row>
+        </Container>
+      );
+    }
+  };
+
+  return onWidthChange();
 };
 
 export default Dashboard;
